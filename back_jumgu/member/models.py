7f4457424b63b6c34bcdcbfbc5bd8f679d454140
Mr.Puttipong Doungvichai <puttipong.dou@gmail.com>
2023-06-29 09:38:10 +0700

    feat : prepare model to sqlite

--- back_jumgu/member/models.py
+++ back_jumgu/member/models.py
@@ -1,3 +1,18 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+#1 py manage.py makemigrations // step prepare model
+#2 py manage.py migrate // step migrate model to db sqlite
+class User(models.Model):
+    id = models.CharField(max_length=40,primary_key=True)
+    pname = models.CharField(max_length=3)
+    fname = models.CharField(max_length=40)
+    lname = models.CharField(max_length=40)
+    email = models.CharField(max_length=80)
+    phone = models.IntegerField(16)
+    countrycode = models.IntegerField(4)
+    image = models.CharField(max_length=100)
+    token = models.CharField(max_length=100)
+    created = models.DateField(auto_now_add=True)
+    lastvisit = models.CharField(max_length=40)
